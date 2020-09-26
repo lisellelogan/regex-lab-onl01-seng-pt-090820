318fa142312475e336aa32fd244fb1bb232cabf4
Liselle Logan <liselle.logan@gmail.com>
2020-09-26 20:13:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  #word[0].scan(/[aeiou]/) ? true : false
-  word.start_with?(/[aeiou]/) ? true : false
-#binding.pry
+  word[0].scan(/[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
